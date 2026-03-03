086c4c275f39e5c2f73fb0eb20b2201e390c7fa7
Guilherme Luiz <guilhermeluiz.dev@gmail.com>
2026-03-03 16:04:51 -0300

    chore: auto-commit worktree changes

---
+++ frontend/jest.config.js
@@ -0,0 +1,20 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: ['/node_modules/(?!.+\\.mjs$)'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config)
